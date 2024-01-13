37dd77c74214fc0d2dfbc5f8d180c41c3715fe6d
cybersemka <91419637+cybersemka@users.noreply.github.com>
2024-01-13 10:35:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,177 +1,309 @@
-// Пример данных о маршрутах (замените данными из вашего приложения)
-let routesData = [
-    { id: 1, name: 'Маршрут 1' },
-    { id: 2, name: 'Маршрут 2' },
-    // Добавьте другие маршруты по аналогии
-];
+const domain = 'http://exam-2023-1-api.std-900.ist.mospolytech.ru';
+const key = 'a3f66b8e-3ca6-481a-8bc8-1052f9a12a1e';
 
-// Пример данных о гидах (замените данными из вашего приложения)
-let guidesData = [
-    { id: 1, name: 'Гид 1' },
-    { id: 2, name: 'Гид 2' },
-    // Добавьте других гидов по аналогии
-];
+const currentPage = 1;
+const pageSize = 10;
 
-// Пример данных о заявках (замените данными из вашего приложения)
-let requestsData = [
-    { id: 1, route: 'Маршрут 1', guide: 'Гид 1', date: '01.01.2022', cost: '1000', status: 'Активна' },
-    { id: 2, route: 'Маршрут 2', guide: 'Гид 2', date: '02.01.2022', cost: '1500', status: 'Активна' },
-    // Добавьте другие заявки по аналогии
-];
+function pluralize(n, content) {
+    let result = content[2];
+    n = Math.abs(n) % 100;
+    let nt = n % 10;
+    if (n >= 10 && n <= 20) result = content[2];
+    else if (nt > 1 && nt < 5) result = content[0];
+    else if (nt == 1) result = content[1];
 
-// Функция для отображения заявок в таблице
-function displayRequests() {
-    const tableBody = document.getElementById('requestsTable');
-    tableBody.innerHTML = ''; // Очистка содержимого таблицы перед обновлением
+    return `${n} ${result}`;
+}
 
-    requestsData.forEach(request => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <th scope="row">${request.id}</th>
-            <td>${request.route}</td>
-            <td>${request.guide}</td>
-            <td>${request.date}</td>
-            <td>${request.cost}</td>
-            <td>${request.status}</td>
-            <td>
-                <button type="button" class="btn btn-info btn-sm" onclick="showDetails(${request.id})">Подробнее</button>
-                <button type="button" class="btn btn-warning btn-sm" onclick="editRequest(${request.id})">Редактировать</button>
-                <button type="button" class="btn btn-danger btn-sm" onclick="deleteRequest(${request.id})">Удалить</button>
+function showModal(guideId, guideName, guidePricePerHour, routeId, routeName, modal) {
+    console.log(guideId, guideName, guidePricePerHour, routeId, routeName, modal);
+    const orderCnt = document.getElementById('order-body');
+    const orderBtn = document.getElementById('order-send');
+
+    orderCnt.innerHTML = '';
+
+    let form = new FormData();
+    form.set('guide_id', guideId);
+    form.set('route_id', routeId);
+    form.set('date', NaN);
+    form.set('duration', 1);
+    form.set('persons', 1);
+    form.set('basePrice', guidePricePerHour);
+
+    let raw = `
+        <div class="row">ФИО гида: ${guideName}</div>
+        <div class="row">Название маршрута: ${routeName}</div>
+        <div class="row">
+            <div>Дата:<div>
+            <input type="date" class="form-control" data-name="date">
+        </div>
+        <div class="row">
+            <div>Время:<div>
+            <input type="time"class="form-control" data-name="time">
+        </div>
+        <div class="row">
+            <div>Длительность:<div>
+            <select class="form-control" data-name="duration">
+                <option value="1" selected>1 Час</option>
+                <option value="2">2 Часа</option>
+                <option value="3">3 Часа</option>
+            </select>
+        </div>
+        <div class="row">
+            <div>Число человек:<div>
+            <input type="number" value="1" class="form-control" min="1" max="20" data-name="persons">
+        </div>
+        <div class="row">
+            <div>Дополнительные опции:<div>
+            <div>
+                <input type="checkbox" id="discountOption" class="form-check-input" data-name="optionFirst">
+                <label for="discountOption" class="form-check-label">Использовать скидку для пенсионеров</label>
+                <p class="form-text">Стоимость уменьшается на 25%</p>
+            </div>
+            <div>
+                <input type="checkbox" id="transferOption" class="form-check-input" data-name="optionSecond">
+                <label for="transferOption" class="form-check-label">Трансфер до ближайших станций метро после экскурсии</label>
+                <p class="form-text">Увеличивает стоимость в выходные на 25%, в будние на 30%</p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div>Cтоимость: <span id="order-price">NaN<span><div>
+        </div>
+
+        <div class="row">
+            <button class="btn btn-primary" id="order-send">Отправить</button>
+            <button class="btn btn-secondary" data-bs-dismiss="modal">Отменить</button>
+        </div>
+    `;
+
+    orderCnt.insertAdjacentHTML("beforeend", raw);
+
+    const discountOption = document.getElementById('discountOption');
+    const transferOption = document.getElementById('transferOption');
+
+    discountOption.onchange = () => {
+        calculateTotalPrice();
+    };
+
+    transferOption.onchange = () => {
+        calculateTotalPrice();
+    };
+
+    orderBtn.onclick = () => {
+        if (isNaN(form.get('price'))) return;
+
+        modal.toggle();
+        addOrder(form);
+    };
+
+    calculateTotalPrice(form);
+}
+
+function displayGuideTable(data, routeData, currentPage, pageSize, totalPages) {
+    document.getElementById('guidesSection').classList.remove('d-none');
+    const cnt = document.getElementById('guides-list');
+    cnt.innerHTML = '';
+    cnt.scrollIntoView({ behavior: 'smooth' });
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = Math.min(startIndex + pageSize, data.length);
+
+    for (let i = startIndex; i < endIndex; i++) {
+        const element = data[i];
+
+        const workExperience = pluralize(
+            element.workExperience,
+            ['года', 'год', 'лет']
+        );
+
+        let row = `
+            <td class="p-2 border">
+                <img src="${element.profileImage}" alt="Profile Image" class="profile-image">
             </td>
-        `;
+            <td class="p-2 border">${element.name}</td>
+            <td class="p-2 border d-none d-sm-table-cell">${element.language}</td>
+            <td class="p-2 border d-none d-md-table-cell">${workExperience}</td>
+            <td class="p-2 border">${element.pricePerHour}р/час</td>
+            <td class="p-2 border">
+                <button class="btn btn-outline-primary" onclick="selectGuide(${element.id}, '${element.name}', ${element.pricePerHour}, ${routeData.id}, '${routeData.name}')">Выбрать</button>
+            </td>`;
+        cnt.insertAdjacentHTML("beforeend", row);
+    }
 
-        tableBody.appendChild(row);
-    });
+    //getGuidesData(routeData, 1, 10) 
+    /*
+    const paginationCnt = document.getElementById('pagination');
+    paginationCnt.innerHTML = '';
+
+    for (let i = 1; i <= totalPages; i++) {
+        const pageBtn = paginationCnt.appendChild(
+            document.createElement('button')
+        );
+        pageBtn.className = `btn btn-outline-primary ${currentPage === i ? 'active' : ''}`;
+        pageBtn.textContent = i;
+        pageBtn.onclick = () => {
+            getGuidesData(routeData, i, pageSize);
+        }
+    }
+    */
 }
 
-// Функция для отображения подробной информации о заявке в модальном окне
-function showDetails(requestId) {
-    const request = requestsData.find(r => r.id === requestId);
+function calculateTotalPrice(form) {
+    const discountOption = document.getElementById('discountOption');
+    const transferOption = document.getElementById('transferOption');
 
-    const detailsModalContent = `
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="detailsModalLabel">Подробная информация</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p><strong>Название маршрута:</strong> ${request.route}</p>
-                    <p><strong>Гид:</strong> ${request.guide}</p>
-                    <p><strong>Дата экскурсии:</strong> ${request.date}</p>
-                    <p><strong>Итоговая стоимость:</strong> ${request.cost}</p>
-                    <p><strong>Статус:</strong> ${request.status}</p>
-                </div>
-            </div>
-        </div>
-    `;
+    const basePrice = form.get('basePrice');
+    let finalPrice = basePrice;
 
-    // Очищаем и обновляем контент модального окна
-    document.getElementById('detailsModal').innerHTML = detailsModalContent;
+    if (discountOption.checked) {
+        finalPrice -= finalPrice * 0.25;
+    }
 
-    // Открываем модальное окно
-    const detailsModal = new bootstrap.Modal(document.getElementById('detailsModal'));
-    detailsModal.show();
+    if (transferOption.checked) {
+        const day = new Date(form.get('date')).getDay();
+        const isWeekend = day === 0 || day === 6;
+
+        if (isWeekend) {
+            finalPrice *= 1.25;
+        } else {
+            finalPrice *= 1.3;
+        }
+    }
+
+    form.set('finalPrice', finalPrice);
+    updateOrderPrice(finalPrice);
 }
 
-// Функция для редактирования заявки
-function editRequest(requestId) {
-    const request = requestsData.find(r => r.id === requestId);
+function selectGuide(guideId, guideName, guidePricePerHour, routeId, routeName, modal) {
+    // Ваш код для обработки выбора гида с указанным ID
+    console.log(`Выбран гид с ID ${guideId}`);
+    // Добавьте здесь логику, которую вы хотите выполнить при выборе гида
 
-    // Здесь можно добавить код для открытия модального окна редактирования заявки
-    // Например, используя $('#editModal').modal('show') и соответствующий HTML-код
-    const editModalContent = `
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="editModalLabel">Редактирование заявки</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form id="editForm">
-                        <div class="mb-3">
-                            <label for="editRoute" class="form-label">Название маршрута</label>
-                            <select class="form-select" id="editRoute" required>
-                                ${generateRouteOptions(request.route)}
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="editGuide" class="form-label">Гид</label>
-                            <select class="form-select" id="editGuide" required>
-                                ${generateGuideOptions(request.guide)}
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="editDate" class="form-label">Дата экскурсии</label>
-                            <input type="text" class="form-control" id="editDate" value="${request.date}" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="editCost" class="form-label">Итоговая стоимость</label>
-                            <input type="text" class="form-control" id="editCost" value="${request.cost}" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Сохранить</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    `;
-
-    // Очищаем и обновляем контент модального окна
-    document.getElementById('editModal').innerHTML = editModalContent;
-
-    // Подписываемся на событие отправки формы
-    document.getElementById('editForm').addEventListener('submit', function (e) {
-        e.preventDefault();
-
-        // Получаем значения из формы
-        const editedRoute = document.getElementById('editRoute').value;
-        const editedGuide = document.getElementById('editGuide').value;
-        const editedDate = document.getElementById('editDate').value;
-        const editedCost = document.getElementById('editCost').value;
-
-        // Обновляем значения в массиве данных
-        request.route = editedRoute;
-        request.guide = editedGuide;
-        request.date = editedDate;
-        request.cost = editedCost;
-
-        // Обновляем таблицу
-        displayRequests();
-
-        // Закрываем модальное окно
-        const editModal = new bootstrap.Modal(document.getElementById('editModal'));
-        editModal.hide();
-    });
-
-    // Открываем модальное окно
-    const editModal = new bootstrap.Modal(document.getElementById('editModal'));
-    editModal.show();
-}
-
-// Функция для удаления заявки
-function deleteRequest(requestId) {
-    const request = requestsData.find(r => r.id === requestId);
-
-    const confirmed = confirm('Вы уверены, что хотите удалить эту заявку?');
+    const selectedGuideButton = document.getElementById(`select-guide-${guideId}`);
+    //selectedGuideButton.classList.add('selected');
     
-    if (confirmed) {
-        // Фиктивно устанавливаем статус "Удалена"
-        request.status = 'Удалена';
+    const orderButton = document.getElementById('confirmOrderBtn');
+    orderButton.classList.remove('d-none');
+    orderButton.onclick = () => {
+        let modal = document.getElementById("guideModal");
+        showModal(guideId, guideName, guidePricePerHour, routeId, routeName, modal);
+    };
+}
 
-        // Обновляем таблицу
-        displayRequests();
+function displayRouteTable(data) {
+    const cnt = document.getElementById('routes-list');
+    cnt.innerHTML = '';
+    for (let element of data) {
+        const routeCnt = cnt.appendChild(
+            document.createElement('tr')
+        );
+
+        let row = `
+            <td class="p-3 border">${element.name}</td>
+            <td class="p-3 d-none d-md-table-cell border">${element.description}</td>
+            <td class="p-3 d-none d-lg-table-cell border">${element.mainObject}</td>`;
+        routeCnt.insertAdjacentHTML("beforeend", row);
+
+        const routeChoise = routeCnt.appendChild(
+            document.createElement('button')
+        );
+        routeChoise.className = 'btn btn-outline-primary';
+        routeChoise.textContent = 'Выбрать';
+        routeChoise.onclick = () => {
+            getGuidesData(element);
+        };
     }
 }
 
-// Функция для генерации HTML-кода с опциями для выбора маршрута
-function generateRouteOptions(selectedRoute) {
-    return routesData.map(route => `<option value="${route.id}" ${route.name === selectedRoute ? 'selected' : ''}>${route.name}</option>`).join('');
+function getRouteData(pageNumber = 1, pageSize = 10) {
+    fetch(
+        `${domain}/api/routes?api_key=${key}`
+    )
+        .then((response) => { 
+            if (!response.ok) { 
+                throw new Error(`Ошибка ${response.status}`); 
+            } 
+            return response.json(); 
+        }) 
+        .then((data) => { 
+            console.log(data); 
+            displayRouteTable(data);
+        })
+        .catch(error => {
+            console.error('Ошибка получения данных о маршрутах:', error);
+        }); 
 }
 
-// Функция для генерации HTML-кода с опциями для выбора гида
-function generateGuideOptions(selectedGuide) {
-    return guidesData.map(guide => `<option value="${guide.id}" ${guide.name === selectedGuide ? 'selected' : ''}>${guide.name}</option>`).join('');
+function getGuidesData(routeData, pageNumber = 1, pageSize = 10) {
+    fetch(
+        `${domain}/api/routes/${routeData.id}/guides?api_key=${key}`
+    )
+        .then((response) => { 
+            if (!response.ok) { 
+                throw new Error(`Ошибка ${response.status}`); 
+            } 
+            return response.json(); 
+        }) 
+        .then((data) => {
+            const totalPages = Math.ceil(data.length / pageSize);
+            displayGuideTable(data, routeData, currentPage, pageSize, totalPages);
+        })
+        .catch(e => console.log(e));
 }
 
-// Вызов функции для отображения заявок при загрузке страницы
-displayRequests();
+function updateOrderPrice(price) {
+    const orderPrice = document.getElementById('order-price');
+    const orderBtn = document.getElementById('order-send');
+
+    if (isNaN(price)) {
+        orderBtn.classList.remove('btn-primary');
+        orderBtn.classList.add('btn-secondary');
+        orderPrice.textContent = 'NaN';
+    } else {
+        orderBtn.classList.add('btn-primary');
+        orderBtn.classList.remove('btn-secondary');
+        orderPrice.textContent = `${price}р`;
+    }
+}
+
+function addOrder(data) {
+    fetch(
+        `${domain}/api/orders?api_key=${key}`,
+        { method: 'POST', body: data}
+    )
+        .then((response) => { 
+            if (!response.ok) { 
+                throw new Error(`Ошибка ${response.status}`); 
+            } 
+            return response.json();
+        }) 
+        .then((data) => {
+            createAlert(
+                'Заявка успешно создана',
+                'success'
+            );
+            console.log(data);
+        })
+        .catch(e => {
+            createAlert(
+                'Во время заполнения заявки произошла ошибка. Попробуйте снова',
+                'warning'
+            );
+        });
+}
+
+function createAlert(text, type) {
+    const cnt = document.getElementById('alerts').appendChild(
+        document.createElement('div')
+    );
+    const raw = `
+        <div class="alert alert-${type} alert-dismissible" role="alert" >
+            <div>${text}</div>
+            <button class="btn-close" data-bs-dismiss="alert"></button>
+        </div>`;
+    cnt.insertAdjacentHTML("beforeend", raw);
+}
+
+getRouteData(currentPage, pageSize);
+//getGuidesData(routeData, currentPage, pageSize);
